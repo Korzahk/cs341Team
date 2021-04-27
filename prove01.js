4462102573696b7a14d1ef78c13027f13a50219f
Korzahk <ecc16001@byui.edu>
2021-04-26 19:17:30 -0700

    npm run prove01 finished

--- prove01.js
+++ prove01.js
@@ -1,6 +1,10 @@
+/*******************************************************************************
+*   CSE 341-02 Web Backend Development II
+*   Spencer Eccles
+*   prove01.js
+*******************************************************************************/
 // cd C:\git\cse341\w01Team\cse341-project
-// node prove01.js
-// localhost:3000/users
+// npm run prove01
 
 const http = require('http');
 const pageNav  = require("C:\\git\\cse341\\w01Team\\cse341-project\\routes\\prove01-routes.js");
@@ -12,7 +16,10 @@ const server = http.createServer((req, res) => {
     }
     if (url === '/users') {
       pageNav.users(res);
-      }
+    }
+    if (url === '/create-user') {
+      pageNav.createUser(req, res);
+    }
 });
 
 server.listen(3000);
--- routes/prove01-routes.js
+++ routes/prove01-routes.js
@@ -1,23 +1,43 @@
+/*******************************************************************************
+*   prove01-routes.js
+*******************************************************************************/
+var newUser = false; // used in functions createUser and users
+
 function forwardSlash(res) {
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Assignment 1</title></head>');
-    res.write(
-      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    res.write('<html><head><title>prove01</title></head>\
+      <body>Greetings!<br><br>username:\
+      <form action="/create-user" method="POST">\
+      <input type="text" name="username">\
+      <button type="submit">Send</button></form></body></html>'
     );
-    res.write('</html>');
     res.end();
-    return forwardSlash;
 }
 
 function users(res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Assignment 1</title></head>');
-    res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
-    res.write('</html>');
-    res.end();
-    return users;
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html><head><title>prove01</title></head>\
+    <body><ul><li>Zezima</li><li>Phatwrecked</li>');
+  if(newUser) { // adds new user to list of users
+    res.write('<li>' + String(newUser) + '</li>');
+  }
+  res.write('</ul></body></html>');
+  res.end();
 }
 
-module.exports = {forwardSlash, users};
+function createUser(req, res) {
+  const body = [];
+  req.on('data', chunk => {
+    body.push(chunk);
+  });
+  req.on('end', () => {
+    const parsedBody = Buffer.concat(body).toString();
+    newUser = parsedBody.split('=')[1];
+    console.log('new user: ' + newUser);
+  });
+  res.statusCode = 302;
+  res.setHeader('Location', '/users');
+  res.end();
+}
+
+module.exports = {forwardSlash, users, createUser};
