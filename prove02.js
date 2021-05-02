1b6987ea98f0b94c4dbaa6726fb643d669e70e6a
Korzahk <ecc16001@byui.edu>
2021-05-01 22:23:52 -0700

    prove02 heroku

--- prove02.js
+++ prove02.js
@@ -9,8 +9,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const PORT = process.env.PORT || 5000
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
 const app = express();
 
 // Route setup
@@ -30,4 +32,4 @@ app.use(express.static(path.join(__dirname, 'public')))
      // 404 page
      res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
    })
-   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+   app.listen(port);
