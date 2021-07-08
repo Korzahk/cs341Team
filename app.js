6c4f4db974ab14311e8458da69421aae9f1666a7
Korzahk <ecc16001@byui.edu>
2021-07-07 17:50:51 -0700

    pr 12

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-// If you downloaded this code, make sure you run `npm install`
-// before trying to start the app.
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 
-const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
-
+const PORT = process.env.PORT || 5000
 const app = express()
-
 const liveChat = require('./routes/liveChat')
 
 app.set('view engine', 'ejs')
@@ -18,11 +14,8 @@ app.set('view engine', 'ejs')
     .use(express.static(path.join(__dirname, 'public')))
     .use(
         session({
-            // Simple and not very secure session
             secret: 'random_text',
-            cookie: {
-                httpOnly: false // Permit access to client session
-            }
+            cookie: { httpOnly: false }
         })
     )
     .use('/', liveChat)
@@ -31,16 +24,13 @@ const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 const io = require('socket.io')(server)
 io.on('connection', socket => {
-    console.log('Client connected!')
-
+    console.log('The Client connected!')
     socket
         .on('disconnect', () => {
             console.log('A client disconnected!')
         })
         .on('newUser', (username, time) => {
-            // A new user logs in.
             const message = `${username} has logged on.`
-            // Tell other users someone has logged on.
             socket.broadcast.emit('newMessage', {
                 message,
                 time,
@@ -48,13 +38,9 @@ io.on('connection', socket => {
             })
         })
         .on('message', data => {
-            // Receive a new message
             console.log('Message received')
             console.log(data)
-            // This one is simple. Just broadcast the data we received.
-            // We can use { ...data } to copy the data object.
-            socket.broadcast.emit('newMessage', {
-                ...data
-            }) // Note, only emits to all OTHER clients, not sender.
+            // emits to all clients other than sender.
+            socket.broadcast.emit('newMessage', { ...data }) 
         })
 })
